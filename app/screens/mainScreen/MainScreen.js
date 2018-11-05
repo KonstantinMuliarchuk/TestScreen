0d5f4c9ea2b38e5049929915ef410441ab43c225
Konstantin Muliarchuk <kotipod@gmail.com>
2018-11-06 01:47:14 +0200

    Refactoring

--- app/screens/mainScreen/MainScreen.js
+++ app/screens/mainScreen/MainScreen.js
@@ -6,21 +6,11 @@ import ImageView from '../../components/imageView';
 import Button from '../../components/button';
 import SegmentController from '../../components/segmentControl';
 import Rows from '../../components/rows';
+import { mainData } from './data';
 
 class MainScreen extends Component {
     render() {
-        const segmentControlData = ['Stock', 'Buy'],
-            mainData = [
-                { title: 'Name', type: 'text', text: 'Office ergonomic chair' },
-                { title: 'Group', type: 'segment', segments: ['Piece', 'Service'] },
-                { title: 'List', type: 'button', text: 'Chairs' },
-                { title: 'Details', type: 'text', text: 'Lorem ipsum dolor sit amet, consectetur'+
-                ' adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+
-                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            },
-                { title: 'Area', type: 'button', text: 'Hong Kong' },
-                { title: 'Fee', type: 'text', text: '765 HKD' },
-            ]
+        const segmentControlData = ['Stock', 'Buy']
         return (
             <View style={styles.container}>
                 <TopNavigator />
